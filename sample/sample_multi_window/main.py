868ce9cdd964922c397d2e7fb0f08c7378765301
akira.mutsuro@furukawaelectric.com <akira.mutsuro@furukawaelectric.com>
2025-03-13 08:01:01 +0900

    sample_multi_window : 終了処理を明示的に実行するよう変更した。

--- sample/sample_multi_window/main.py
+++ sample/sample_multi_window/main.py
@@ -1,11 +1,13 @@
+import sys
 import tkinter as tk
 
 from sub import create_sub_window
 
 
-def main():
+def main() -> None:
     def on_close() -> None:
         win_root.destroy()
+        sys.exit()
 
     def show_sub_window() -> None:
         win_root.withdraw()
@@ -13,13 +15,13 @@ def main():
         win_sub.focus_force()
 
     win_root = tk.Tk()
-    win_root.title("Main Window")
-    win_root.geometry("400x300")
-    win_root.resizable(False, False)
-    win_root.protocol("WM_DELETE_WINDOW", func=on_close)
+    win_root.title(string="Main Window")
+    win_root.geometry(newGeometry="400x300")
+    win_root.resizable(width=False, height=False)
+    win_root.protocol(name="WM_DELETE_WINDOW", func=on_close)
     win_root.focus_force
 
-    button = tk.Button(win_root, text="Open Sub Window", command=show_sub_window)
+    button = tk.Button(master=win_root, text="Open Sub Window", command=show_sub_window)
     button.pack(padx=10, pady=10)
 
     win_root.mainloop()
--- sample/sample_multi_window/sub.py
+++ sample/sample_multi_window/sub.py
@@ -2,17 +2,17 @@ import tkinter as tk
 
 
 def create_sub_window(win_parent: tk.Tk) -> tk.Toplevel:
-    def on_close():
+    def on_close() -> None:
         win_me.destroy()
         win_parent.deiconify()
         win_parent.focus_force
 
     win_me = tk.Toplevel()
-    win_me.title("Sub Window")
-    win_me.geometry("400x300")
-    win_me.resizable(False, False)
-    win_me.protocol("WM_DELETE_WINDOW", on_close)
-    label = tk.Label(win_me, text="This is the sub window")
+    win_me.title(string="Sub Window")
+    win_me.geometry(newGeometry="400x300")
+    win_me.resizable(width=False, height=False)
+    win_me.protocol(name="WM_DELETE_WINDOW", func=on_close)
+    label = tk.Label(master=win_me, text="This is the sub window")
     label.pack(pady=20)
     return win_me
 
@@ -20,16 +20,16 @@ def create_sub_window(win_parent: tk.Tk) -> tk.Toplevel:
 # お約束のおまじない
 if __name__ == "__main__":
 
-    def show_sub_window():
+    def show_sub_window() -> None:
         win_root.withdraw()
-        win_sub = create_sub_window(win_root)
+        win_sub: tk.Toplevel = create_sub_window(win_parent=win_root)
         win_sub.focus_force()
 
     win_root = tk.Tk()
-    win_root.title("Dummy Main Window")
-    win_root.geometry("400x300")
-    win_root.resizable(False, False)
+    win_root.title(string="Dummy Main Window")
+    win_root.geometry(newGeometry="400x300")
+    win_root.resizable(width=False, height=False)
     win_root.focus
-    button = tk.Button(win_root, text="Open Sub Window", command=show_sub_window)
+    button = tk.Button(master=win_root, text="Open Sub Window", command=show_sub_window)
     button.pack(padx=10, pady=10)
     win_root.mainloop()
