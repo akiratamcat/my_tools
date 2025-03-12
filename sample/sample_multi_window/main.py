ed1e5ca3b18d06bf925e4bfcb8e3ebd93ed23594
akira.mutsuro@furukawaelectric.com <akira.mutsuro@furukawaelectric.com>
2025-03-12 15:26:31 +0900

    sample_multi_window : メインウィンドウの close 処理追加。

--- sample/sample_multi_window/main.py
+++ sample/sample_multi_window/main.py
@@ -4,15 +4,19 @@ from sub import create_sub_window
 
 
 def main():
-    def show_sub_window():
+    def on_close() -> None:
+        win_root.destroy()
+
+    def show_sub_window() -> None:
         win_root.withdraw()
-        win_sub = create_sub_window(win_root)
+        win_sub: tk.Toplevel = create_sub_window(win_parent=win_root)
         win_sub.focus_force()
 
     win_root = tk.Tk()
     win_root.title("Main Window")
     win_root.geometry("400x300")
     win_root.resizable(False, False)
+    win_root.protocol("WM_DELETE_WINDOW", func=on_close)
     win_root.focus_force
 
     button = tk.Button(win_root, text="Open Sub Window", command=show_sub_window)
